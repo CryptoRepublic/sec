e19a4a525a3a1fe74917694df605185e1d46068a
CryptoRepublic <59357352+CryptoRepublic@users.noreply.github.com>
2024-06-22 20:29:54 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-    // const helmet = require('helmet');
+    const helmet = require('helmet');
     const rateLimit = require('express-rate-limit');
 
 const app = express();
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 3000;
 
 // Use Helmet to set various HTTP headers for security
 //app.use(helmet());
+
+// Imposta manualmente gli header di sicurezza, escludendo X-Frame-Options
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    res.setHeader('Referrer-Policy', 'no-referrer');
+    res.setHeader('Content-Security-Policy', "default-src 'self'");
+    next();
+});
 
 // Rate limiting to prevent brute force attacks
 const apiLimiter = rateLimit({
